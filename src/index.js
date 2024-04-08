661ce50121a6fb8626e6255029f0dc58b075d4f7
MoonstruckDev <dev@moonstruck.pro>
2024-04-08 12:56:13 +0200

    Attempt #2 Deploy react on GH pages

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/kasa">
       <App />
     </BrowserRouter>
   </StrictMode>
